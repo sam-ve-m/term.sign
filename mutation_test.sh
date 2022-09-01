6aee26c54919213f457c79d56c204a3b82893f45
Samuel Veloso Marçal <svm@lionx.com.br>
2022-09-01 14:10:28 -0300

    chore: python_path_in_mutatest_script

--- mutation_test.sh
+++ mutation_test.sh
@@ -1,1 +1,2 @@
-mutatest -s ./func -y 'if' 'nc' 'ix' 'su' 'bs' 'bc' 'bn' -x 60 -n 1000 -t 'python3 -m pytest --cov-report term-missing --cov-config=.coveragerc --cov=func'
+export PYTHONPATH=$PWD/func
+mutatest -s ./func -y 'if' 'nc' 'ix' 'su' 'bs' 'bc' 'bn' -x 60 -n 1000 -t 'python3 -m pytest --cov-report term-missing --cov-config=.coveragerc --cov=func -v'
