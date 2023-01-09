016306e90d96e95261162e06e96021cf8444f855
Samuel Veloso Marçal <samuel.marcal@ligainvest.com.br>
2023-01-09 14:12:44 -0300

    build: created standard image

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 fission spec init
-fission env create --name term-sign-env --image nexus.sigame.com.br/term-sign:0.4.0 --poolsize 0 --version 3 --imagepullsecret "nexus-v3" --spec
+fission env create --name term-sign-env --image nexus.sigame.com.br/fission-term-sign:0.1.0 --poolsize 0 --version 3 --imagepullsecret "nexus-v3" --spec
 fission fn create --name term-sign-fn --env term-sign-env --code fission.py --targetcpu 80 --executortype newdeploy --maxscale 3 --requestsperpod 10000 --spec
 fission route create --name term-sign-rt --method PUT --url /term/sign --function term-sign-fn --spec
